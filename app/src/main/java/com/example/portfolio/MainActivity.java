8f5cf90e9f33df93aa9c91c2fc209db370c68d9b
Mayur500000 <142081670+Mayur500000@users.noreply.github.com>
2023-08-27 23:39:44 +0530

    added ProjectActivity

--- app/src/main/java/com/example/portfolio/MainActivity.java
+++ app/src/main/java/com/example/portfolio/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.portfolio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button calButton = findViewById(R.id.projets);
+        calButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button Working!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, ProjectActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
