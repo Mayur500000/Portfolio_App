2c2debcc727383ab4b2309ec853d0d8af101a01f
Mayur500000 <142081670+Mayur500000@users.noreply.github.com>
2023-08-29 20:32:40 +0530

    29

--- app/src/main/java/com/example/portfolio/BasicScreen.java
+++ app/src/main/java/com/example/portfolio/BasicScreen.java
@@ -2,13 +2,31 @@ package com.example.portfolio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class BasicScreen extends AppCompatActivity {
-
+    Button heightButton,luckynumberButton2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_screen);
+        heightButton = findViewById(R.id.heightButton);
+        luckynumberButton2 =findViewById(R.id.luckynumberButton);
+
+
+    }
+    public void heightBtn(View view){
+        Toast.makeText(BasicScreen.this,"Height Calulator", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(BasicScreen.this, HeightCalActivity.class );
+        startActivity(intent);
+    }
+    public void luckyBtn(View view){
+        Toast.makeText(BasicScreen.this,"Height Calulator", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(BasicScreen.this, LuckyNoBasic.class );
+        startActivity(intent);
     }
 }
