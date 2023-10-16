07e2ccd434fe631fab869a10803a0d979b9967d3
Mayur500000 <142081670+Mayur500000@users.noreply.github.com>
2023-10-16 11:16:08 +0530

    16/10

--- app/src/main/java/com/example/portfolio/Intermediate.java
+++ app/src/main/java/com/example/portfolio/Intermediate.java
@@ -2,13 +2,25 @@ package com.example.portfolio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Intermediate extends AppCompatActivity {
+    Button brating ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intermediate);
+        brating = findViewById(R.id.rating);
+        brating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intermediate.this, RatingPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
